c8523244da581fde71a4f231fb8148599194c9c9
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-15 23:52:07 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def my_all?(collection) # does not call on all?
 i = 0
-  block_return_values = [] #
+  block_return_values = [] #can handle an empty collection
   while i < collection.length
     block_return_values << yield(collection[i])
     i = i + 1
