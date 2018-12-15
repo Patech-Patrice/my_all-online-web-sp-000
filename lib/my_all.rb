78648313f4328dd9749f87677d7cb9d7f9e7bec5
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-15 23:52:31 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection) # does not call on all?
 i = 0
   block_return_values = [] #can handle an empty collection
   while i < collection.length
-    block_return_values << yield(collection[i])
+    block_return_values << yield(collection[i]) # yields the correct element to the block
     i = i + 1
   end
  
